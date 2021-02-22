2e5332da00a3abaa81c535692ddc26693c51d67a
T-Aruga <baideneo@gmail.com>
2021-02-22 23:15:58 +0900

    Add radio button

--- front/src/components/Basic.tsx
+++ front/src/components/Basic.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { TextField } from "@material-ui/core";
+import {
+  TextField,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+ } from "@material-ui/core";
 
 import useStyles from "./styles";
 
 import { RootState } from "../domain/entity/rootState";
 
+import { Gender } from "../domain/entity/gender";
 import { Profile } from "../domain/entity/profile";
 
 import { PROFILE } from "../domain/services/profile";
@@ -39,6 +47,24 @@ const Basic = () => {
         value={profile.description}
         onChange={e => handleChange({ description: e.target.value })}
       />
+      <FormControl className={classes.formField}>
+        <FormLabel>{PROFILE.GENDER}</FormLabel>
+        <RadioGroup
+          value={profile.gender}
+          onChange={e => handleChange({ gender: e.target.value as Gender })}
+        >
+          <FormControlLabel
+            value="male"
+            label="男性"
+            control={<Radio color="primary" />}
+          />
+          <FormControlLabel
+            value="female"
+            label="女性"
+            control={<Radio color="primary" />}
+          />
+        </RadioGroup>
+      </FormControl>
     </>
   );
 };
